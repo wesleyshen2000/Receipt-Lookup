e8d5d5893d2ccacd472501833d93a22b96a17931
Leon <lshen1905@gmail.com>
2021-05-09 20:42:05 -0600

    Image downloading feature added

--- src/Data/Data.py
+++ src/Data/Data.py
@@ -1,45 +1,73 @@
 #Pull the data from the receipt and organize it by item: quantity
 import requests
 import json
-def read_file():
-    """
-    Reads the config file to get parameter information
-    :return: list of parameters for request
-    :rtype: dict
-    """
-    param = {}
-    with open('../../config/config.txt') as f:
-        for line in f:
-            if line[0] != '#':
-                line = line.replace(':','')
-                line = line.rstrip('\n')
-                result = line.split(' ')
-                param[result[0]] = result[1]
+from FileManager import FileManager
+class Data:
+    def __init__(self):
+        self.fm = FileManager()
+        self.tmp_path = "../../tmp/"
 
-    return param
+    def read_file(self):
+            """
+            Reads the config file to get parameter information
+            :return: list of parameters for request
+            :rtype: dict
+            """
+            param = {}
+            with open('../../config/config.txt') as f:
+                for line in f:
+                    if line[0] != '#':
+                        line = line.replace(':','')
+                        line = line.rstrip('\n')
+                        result = line.split(' ')
+                        param[result[0]] = result[1]
 
-def create_request():
-    """
-    Creates an initial request to gather data
-    :return: All the items bought from Walmart
-    :rtype: dict
-    """
-    data = read_file()
+            return param
 
-    url = 'https://www.walmart.com/chcwebapp/api/receipts'
-    response = requests.post(url,data=data).text
-    response_dict =  json.loads(response)
-    items = response_dict['receipts'][0]['items'] #Finds the items category
-    results = {}
-    for item in items:
-        if item['upc'] in results:
-            results[item['upc']]['quantity']+= 1
+    def create_request(self):
+        """
+        Creates an initial request to gather data
+        :return: All the items bought from Walmart
+        :rtype: dict
+        """
+        data = self.read_file()
+
+        #Check if tmp folder is created, if not create folder
+        try:
+            self.fm.create_folder(self.tmp_path)
+        except FileExistsError:
+            print("TMP Folder already exists")
+
+        #Walmart Request
+        url = 'https://www.walmart.com/chcwebapp/api/receipts'
+        response = requests.post(url,data=data).text
+        response_dict =  json.loads(response)
+        items = response_dict['receipts'][0]['items'] #Finds the items category
+        results = {}
+        for item in items:
+            if item['upc'] in results:
+                results[item['upc']]['quantity']+= 1
+            else:
+                #Adds new item into dictionary
+                results[item['upc']] = {}
+                results[item['upc']]['description']= item['description']
+                results[item['upc']]['price'] = item['price']
+                results[item['upc']]['quantity']= item['quantity']
+
+                #Download image
+                try:
+                    #results[item['upc']]['imageUrl'] = item['imageUrl']
+                    self.download_image(item['imageUrl'],self.tmp_path+item['description']+".png")
+                except:
+                    results[item['upc']]['imageUrl'] = None
+
+    def download_image(self,url,path):
+        if not self.fm.file_exist(path):
+            file = open(path,'wb')
+            response = requests.get(url)
+            file.write(response.content)
         else:
-            results[item['upc']] = {}
-            results[item['upc']]['description']= item['description']
-            results[item['upc']]['price'] = item['price']
-            results[item['upc']]['quantity']= item['quantity']
-    print(results)
-    print(type(results['007874223747']['quantity']))
-
-create_request()
+            print("{} already exists".format(path))
+test = Data()
+test.create_request()
+#download_image("test","test","../../tmp/")
---
+++ src/Data/FileManager.py
@@ -0,0 +1,23 @@
+import os
+class FileManager:
+    """
+    Manages files and directories
+    """
+
+    def create_folder(self,path):
+        try:
+            os.mkdir(path)
+        except FileExistsError:
+            print("File Already Exists")
+
+    def delete_folder(self,path):
+        os.rmdir(path)
+
+    def file_exist(self,path):
+        if os.path.exists(path):
+            return True
+        return False
+#f = FileManager()
+#f.create_folder("../../tmp")
+#f.delete_folder("../../tmp/")
+
